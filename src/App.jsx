32bf28db97aeef3265886e17c42b7714a4c71c90
uaeboss <jeskemal@gmail.com>
2023-07-03 10:48:09 +0200

    added grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,16 @@ function App() {
 
   return (
     <>
-      <div className='wrapper'></div>
+      <div className='wrapper'>
+      <div class="container">
+  <div class="logo"></div>
+  <div class="home"></div>
+  <div class="navbar"></div>
+  <div class="login"></div>
+  <div class="content"></div>
+  <div class="footer"></div>
+</div>
+      </div>
       <div></div>
     </>
   )
