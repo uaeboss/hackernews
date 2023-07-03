d3fa61ae0acfeaec32e153f8c08fbd83fabe820f
uaeboss <jeskemal@gmail.com>
2023-07-03 11:52:44 +0200

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-import { useState } from 'react'
-import logo from './images/y18.svg'
+import { useState } from "react";
+import logo from "./images/y18.svg";
+import Navbar from "./components/Navbar";
 function App() {
-
   return (
     <>
-      <div className='wrapper'>
-      <div class="container">
-  <div class="logo">
-    <img src={logo} alt='Logo'/>
-  </div>
-  <div class="home"></div>
-  <div class="navbar"></div>
-  <div class="login"></div>
-  <div class="content"></div>
-  <div class="footer"></div>
-</div>
+      <div className="wrapper">
+        <div class="container">
+          <div class="logo">
+            <img src={logo} alt="Logo" />
+          </div>
+          <div class="home">
+            <p className="black">Hacker News</p>
+          </div>
+          <div class="navbar">
+            <Navbar />
+          </div>
+          <div class="login"></div>
+          <div class="content"></div>
+          <div class="footer"></div>
+        </div>
       </div>
       <div></div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
