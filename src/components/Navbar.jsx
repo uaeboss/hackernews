a04731306a31ee17f3e20a38de85d8ac025ebb9d
uaeboss <jeskemal@gmail.com>
2023-07-03 12:17:11 +0200

    title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ function Navbar() {
             <ul>
                 <li><a href="#new">new</a> &#124;</li>
                 <li>&nbsp;<a href="#past">past</a> &#124;</li>
-                <li>&nbsp;<a href="#new">comments</a> &#124;</li>
-                <li>&nbsp;<a href="#new">ask</a> &#124;</li>
-                <li>&nbsp;<a href="#new">show</a> &#124;</li>
-                <li>&nbsp;<a href="#new">jobs</a> &#124;</li>
-                <li>&nbsp;<a href="#new">submit</a></li>
+                <li>&nbsp;<a href="#comments">comments</a> &#124;</li>
+                <li>&nbsp;<a href="#ask">ask</a> &#124;</li>
+                <li>&nbsp;<a href="#show">show</a> &#124;</li>
+                <li>&nbsp;<a href="#jobs">jobs</a> &#124;</li>
+                <li>&nbsp;<a href="#submit">submit</a></li>
             </ul>
         </nav>
         </>
